8b7315362cd66e47b870822c2b7ffc2ce89887a3
knighthot <fikramulbizli@gmail.com>
2024-05-07 01:42:22 +0700

    upload last v1.1 revisi

--- src/components/Black2.jsx
+++ src/components/Black2.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect,useState } from 'react';
 import  '../assets/css/Black2.css';
 import fikram from '../assets/image/fikramwithbingkai.png';
-
+import { Link } from 'react-router-dom';
 const Black1 = () => {
  
 
@@ -50,6 +50,9 @@ const Black1 = () => {
             <p className="center-text-black2" id='small-font'>you have cleared all the levels !!you can go back to any level you wantor contact me for work or maybe we can have a cup of coffee and discuss about more games !! </p>
             <a href="https://drive.google.com/drive/folders/1McG-lKd0tibRqroixRfYPr0HK4EkWdnZ?usp=sharing" download>
                 <button className="download-button">Download CV</button>
+                 <Link to="/">
+                <button className="download-button" id='back'>Back to Maingate</button>
+            </Link>
             </a>
         </div>
     );
